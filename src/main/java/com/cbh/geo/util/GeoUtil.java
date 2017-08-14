bde638555b4e846e91742f65aaa1657241a1c215
tommy770221 <tommy770221@gmail.com>
2017-08-15 00:18:28 +0800

    1.醫院地址定位 2.備份mongodb

--- src/main/java/com/cbh/geo/util/GeoUtil.java
+++ src/main/java/com/cbh/geo/util/GeoUtil.java
@@ -17,9 +17,11 @@ public class GeoUtil {
         OkHttpClient client = new OkHttpClient();
 
         Request request = new Request.Builder()
-                .url("http://maps.googleapis.com/maps/api/geocode/json?address="+ URLEncoder.encode(address,"UTF8")+"&sensor=false")
+                .url("https://maps.googleapis.com/maps/api/geocode/json?address="+ URLEncoder.encode(address,"UTF8")+"&key=AIzaSyCF0oh0dPvdTbgCs-Nrlc0J8cKqCC72cA4")
                 .get()
                 .addHeader("cache-control", "no-cache")
+                .addHeader("User-Agent","Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0")
+                .addHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
                 .build();
 
         Response response = client.newCall(request).execute();
